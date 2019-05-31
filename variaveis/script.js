98c0d2bbe4dc125895282f346a099c56fc33f2a7
LeonardoCesca <leonardocescaf@gmail.com>
2019-05-31 10:06:17 -0300

    ADD new file into journey

---
+++ variaveis/script.js
@@ -0,0 +1,18 @@
+// Declarar uma variável com o seu nome
+var name = 'Leonardo';
+
+// Declarar uma variável com a sua idade
+var age = 21;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+var favoriteFood;
+
+// Atribuir valor a sua comida favorita
+var favoriteFood = 'Sorvete';
+
+// Declarar 5 variáveis diferentes sem valores
+var time,
+    school,
+    pais,
+    month,
+    days;
