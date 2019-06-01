a0e71c0378447e08a0fd0620a0b476a0be45d644
LeonardoCesca <leonardocescaf@gmail.com>
2019-06-01 15:59:08 -0300

    ADD more exercuse

---
+++ tipos-dados/script.js
@@ -0,0 +1,15 @@
+// Declare uma variável contendo uma string
+let name = 'Leonardo';
+// Declare uma variável contendo um número dentro de uma string
+let nameAndAge = 'Leonardo 21';
+// Declare uma variável com a sua idade
+let age = 21;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let firstName = 'Leonardo';
+let lastName = 'Cesca';
+let sumNames = firstName + ' ' + lastName;
+// Coloque a seguinte frase em uma variável: It's time
+let phrase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+typeof(firstName);
