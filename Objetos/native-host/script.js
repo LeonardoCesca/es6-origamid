2a9b4ac437671acce96d960e97462b6e3bb8514a
LeonardoCesca <leonardocescaf@gmail.com>
2019-06-15 13:21:01 -0300

    ADD native hosts

---
+++ Objetos/native-host/script.js
@@ -0,0 +1,21 @@
+// Liste 5 objetos nativos
+Object 
+String
+Array
+Function
+
+// Liste 5 objetos do browser
+Window
+History
+Document
+HTMLCollection
+NodeList
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+if(typeof document.webkitHidden !== undefined) {
+    console.log('Existe');
+} else {
+    console.log ('Não existe');
+}
