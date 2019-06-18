083b80a6da2e6edd1094496380720ba4680fa1d7
LeonardoCesca <leonardocescaf@gmail.com>
2019-06-18 13:11:14 -0300

    ADD Arrays Interactions classroom

--- Objetos/arrays-interactions/script.js
+++ Objetos/arrays-interactions/script.js
@@ -1,17 +1,40 @@
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
+const allCourses = document.querySelectorAll('.curso');
+const arrayCourses = Array.from(allCourses);
 
+const objectCourses = arrayCourses.map((course) => {
+  const title = course.querySelector('h1').innerText;
+  const desc = course.querySelector('p').innerText;
+  const classrooms = course.querySelector('.aulas').innerText;
+  const hours = course.querySelector('.horas').innerText;
+
+  return {
+    titulo: title,
+    descricao: desc,
+    aula: classrooms,
+    horas: hours
+  }
+})
+console.log(objectCourses);
 
 // Retorne uma lista com os
 // números maiores que 100
 const numeros = [3, 44, 333, 23, 122, 322, 33];
+const moreThan100 = numeros.filter(num => num > 100);
 
+console.log(moreThan100);
 
 // Verifique se Baixo faz parte
 // da lista de instrumentos e retorne true
 const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
 
+const isTrue = instrumentos.some((item) => {
+  return item === 'Baixo';
+});
+
+console.log(isTrue);
 
 // Retorne o valor total das compras
 const compras = [{
@@ -35,3 +58,10 @@ const compras = [{
     preco: 'R$ 10,60'
   }
 ]
+
+let totalPrice = 0;
+compras.forEach((item) => {
+  const itemPrice = +item.preco.replace('R$', '').trim().replace(',', '.');
+  totalPrice = totalPrice + itemPrice;
+})
+console.log(totalPrice);
