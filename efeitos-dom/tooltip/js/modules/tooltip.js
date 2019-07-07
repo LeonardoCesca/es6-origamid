a15e4520b2f1385e9de4a43330c2d48c74d9b49f
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-07 12:05:57 -0300

    ADD finish tooltip lesson

--- efeitos-dom/tooltip/js/modules/tooltip.js
+++ efeitos-dom/tooltip/js/modules/tooltip.js
@@ -1,4 +1,4 @@
-export default function initTooltip() {}
+export default function initTooltip() { }
 
 const toolTips = document.querySelectorAll('[data-tooltip]');
 
@@ -7,8 +7,31 @@ toolTips.forEach((item) => {
 })
 
 function onMouserOver(event) {
-    criarTooltipBox(this);
-    console.log(event);
+    const tooltipBox = criarTooltipBox(this);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove);
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+}
+
+const onMouseLeave = {
+    tooltipBox: '',
+    element: '',
+    handleEvent() {
+        this.tooltipBox.remove();
+        this.element.removeEventListener('mouseleave', onMouseLeave);
+        this.element.removeEventListener('mousemove', onMouseMove);
+    }
+}
+
+const onMouseMove = {
+    handleEvent(event) {
+        this.tooltipBox.style.top = event.pageY + 20 + 'px';
+        this.tooltipBox.style.left = event.pageX + 20 + 'px';
+    }
 }
 
 function criarTooltipBox(element) {
@@ -17,5 +40,5 @@ function criarTooltipBox(element) {
     tooltipBox.classList.add('tooltip');
     tooltipBox.innerText = text;
     document.body.appendChild(tooltipBox);
-    console.log(element);
+    return tooltipBox;
 }
