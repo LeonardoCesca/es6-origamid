5b3bf29dda7c81b19710a73cac890a2006ba5b45
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-07 20:24:38 -0300

    ADD setTimeOut lesson

--- efeitos-dom/set-timeout/script-aula.js
+++ efeitos-dom/set-timeout/script-aula.js
@@ -11,3 +11,26 @@ for (let i = 0; i < 20; i++) {
         console.log(i);
     }, 1000 * i);
 }
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', handleClick);
+
+function handleClick() {
+    setTimeout(() => {
+        this.classList.add('active');
+    }, 1000);
+}
+
+function loop(texto) {
+    console.log(texto);
+}
+setInterval(loop, 300, '300ms');
+
+let i = 0;
+const meuLoop = setInterval(() => {
+    console.log(i++);
+    if(i > 20) {
+        clearInterval(meuLoop);
+    }
+}, 300);
