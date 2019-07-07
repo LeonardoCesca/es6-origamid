f72c38159120f3dcfe42d06b780597316488e29f
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-07 16:36:27 -0300

    ADD dropdown lesson (unfinish)

--- efeitos-dom/click-outside/js/modules/drop-down.js
+++ efeitos-dom/click-outside/js/modules/drop-down.js
@@ -1,4 +1,4 @@
-export default function initDropDown() {}
+export default function initDropDown() { }
 
 const dropdownMenu = document.querySelectorAll('[data-dropdown]');
 dropdownMenu.forEach(menu => {
@@ -9,5 +9,30 @@ dropdownMenu.forEach(menu => {
 
 function handleClick(event) {
     event.preventDefault();
-    this.classList.toggle('active');
+    this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+        this.classList.remove('active');
+    });
+}
+
+function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside';
+
+    if (!element.hasAttribute(outside)) {
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handleOutSideClick);
+        })
+        element.setAttribute(outside, '');
+    }
+
+    function handleOutSideClick(event) {
+        if (!element.contains(event.target)) {
+            element.removeAttribute(outside);
+            events.forEach(userEvent => {
+                html.removeEventListener(userEvent, handleOutSideClick);
+            })
+            callback();
+        }
+    }
 }
