60910511f47b8cd9a54873711a0f1c771fefd4b0
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-07 21:46:47 -0300

    ADD homework setTimeOut

--- efeitos-dom/set-timeout/script.js
+++ efeitos-dom/set-timeout/script.js
@@ -6,3 +6,30 @@ setInterval(changeColor, 2000);
 
 // Crie um cronometro utilizando o setInterval. Deve ser possível
 // iniciar, pausar e resetar (duplo clique no pausar).
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const time = document.querySelector('.time');
+
+start.addEventListener('click', startTime);
+pause.addEventListener('click', pauseTime);
+pause.addEventListener('dblclick', resetTime);
+
+let i = 0;
+let timer;
+
+function startTime() {
+    timer = setInterval(() => {
+        time.innerText = i++;
+    }, 100);
+    start.setAttribute('disabled', '');
+}
+
+function pauseTime() {
+    clearInterval(timer);  
+    start.removeAttribute('disabled');
+}
+
+function resetTime() {
+    time.innerText = 0;
+    i = 0;
+}
