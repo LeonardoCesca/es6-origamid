82f66778e1520ab534563403955cd3bbfe58a664
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-08 15:59:16 -0300

    ADD forms (finish)

--- efeitos-dom/forms/script.js
+++ efeitos-dom/forms/script.js
@@ -1,9 +1,36 @@
 const contato = document.querySelector('#contato');
 const texto = document.querySelector('.texto');
+const dados = {};
 
 function handleKeyUp(event) {
-    console.log(event.target.value);
-    texto.innerText = event.target.value; 
+    const target = event.target;
+    const texto = document.querySelector('.texto');
+
+    if(!target.checkValidity()) {    
+        target.classList.add('invalido');
+        target.nextElementSibling.innerText = target.validationMessage;
+    }
+        console.log(event.target.checkValidity());
+        console.log(event.target.value);
+        texto.innerText = event.target.value; 
+}
+
+function changeBackground(event) {
+    document.body.style.backgroundColor = event.target.value;
+}   
+
+function checkBox(event) {
+    if(event.target.checked) {
+        console.log(event.target.value);
+    }
+}
+
+function handleChange(events) {
+    dados[event.target.name] = event.target.value;
+    console.log(dados);
 }
 
 contato.addEventListener('change', handleKeyUp);
+contato.addEventListener('change', changeBackground);
+contato.addEventListener('change', checkBox);
+contato.addEventListener('change', handleChange);
