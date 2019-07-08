46e5cc76f98fba14bb7443b118f9ba163ed926c4
LeonardoCesca <leonardocescaf@gmail.com>
2019-07-08 14:18:16 -0300

    ADD forms I lesson

--- efeitos-dom/forms/script.js
+++ efeitos-dom/forms/script.js
@@ -1,1 +1,9 @@
-console.log(document.forms);
+const contato = document.querySelector('#contato');
+const texto = document.querySelector('.texto');
+
+function handleKeyUp(event) {
+    console.log(event.target.value);
+    texto.innerText = event.target.value; 
+}
+
+contato.addEventListener('change', handleKeyUp);
