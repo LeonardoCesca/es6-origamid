2f044a98f5cb96658b5c4c0daa1edd19655b4806
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-06 11:18:51 -0300

    ADD promise.all method

--- javascript-assincrono/promises/script.js
+++ javascript-assincrono/promises/script.js
@@ -1,23 +1,41 @@
-const promise = new Promise((resolve, reject) => {
-    let condition = true;
-    if(condition) {
-        setTimeout(() =>{
-            resolve('Estou pronto');
-        }, 1000);
-    } else {
-        reject(Error('Um erro ocorreu'));
-    }
-});
+// const promise = new Promise((resolve, reject) => {
+//     let condition = true;
+//     if(condition) {
+//         setTimeout(() =>{
+//             resolve('Estou pronto');
+//         }, 1000);
+//     } else {
+//         reject(Error('Um erro ocorreu'));
+//     }
+// });
 
-const returned = promise.then((resolved) => { 
-    console.log(resolved); 
-    return 'oi';
-}).then((resolved) => {
-    console.log(resolved);
-}).catch((reject) => {
-    console.log(reject);
-}).finally(() => {
-    console.log('Finish')
+// const returned = promise.then((resolved) => { 
+//     console.log(resolved); 
+//     return 'oi';
+// }).then((resolved) => {
+//     console.log(resolved);
+// }).catch((reject) => {
+//     console.log(reject);
+// }).finally(() => {
+//     console.log('Finish')
+// })
+
+// console.log(returned);
+
+const login = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('User ok!')
+    }, 1000)
 })
 
-console.log(returned);
+const data = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('Data ok!')
+    }, 1500)
+})
+
+const ok = Promise.all([login, data]);
+
+ok.then((resolve) => {
+    console.log(resolve);
+})
