4fa091b3c7864447c852b5c1b029274bc3b89883
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-22 19:03:36 -0300

    ADD returning the url with fetch

--- javascript-assincrono/fetch/script.js
+++ javascript-assincrono/fetch/script.js
@@ -1,7 +1,7 @@
 const archive = fetch('./docs.txt');
 
 archive.then(response => {
-    console.log(response.status)
+    console.log(response.url)
     if(response.status === 404) {
         console.log('Página não existe')
     }
