72cb8f960733ae536fb8054df609674ae0dec985
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-22 18:58:51 -0300

    ADD response status

--- javascript-assincrono/fetch/script.js
+++ javascript-assincrono/fetch/script.js
@@ -1,6 +1,5 @@
 const cep = fetch('https://viacep.com.br/ws/01001000/json/');
 
 cep.then(response => {
-    console.log(response)
-    response.headers.forEach(console.log)
+    console.log(response.status)
 })
