85fbd4a0f42b35981b3b7ade9ed1bdd95c10e068
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-22 19:10:24 -0300

    ADD type into fetch

--- javascript-assincrono/fetch/script.js
+++ javascript-assincrono/fetch/script.js
@@ -1,8 +1,13 @@
 const archive = fetch('./docs.txt');
 
 archive.then(response => {
-    console.log(response.url)
+    console.log(response.type)
     if(response.status === 404) {
         console.log('Página não existe')
     }
 })
+
+/* Types:
+    - basic: significa requisição para dentro do próprio servidor 
+    - cors: requisição para outro servidor mas é permitida
+*/
