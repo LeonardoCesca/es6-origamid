585f3895c657d49b3fe95fb81c5331f178af8056
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-23 14:34:52 -0300

    ADD lesson 1 - fetch homework

--- javascript-assincrono/fetch/homework.js
+++ javascript-assincrono/fetch/homework.js
@@ -1,6 +1,25 @@
 // Utilizando a API https://viacep.com.br/ws/${CEP}/json/
 // crie um formulário onde o usuário pode digitar o cep
 // e o endereço completo é retornado ao clicar em buscar
+const inputCep = document.getElementById('cep')
+const btnCep = document.getElementById('btn-cep')
+const resultadoCep = document.querySelector('.resultadoCep')
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    const cep = inputCep.value
+    buscaCep(cep)
+}
+
+function buscaCep(cep) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.text())
+    .then(body => {
+        resultadoCep.innerText = body
+    })
+}
 
 // Utilizando a API https://blockchain.info/ticker
 // retorne no DOM o valor de compra da bitcoin and reais.
