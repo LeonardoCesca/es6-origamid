c214057f1468c63084cb06dac553c348188a9440
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-24 14:06:09 -0300

    ADD into localStorage JSON

--- javascript-assincrono/json/script.js
+++ javascript-assincrono/json/script.js
@@ -2,7 +2,6 @@ fetch('./dados.json')
 .then(response => response.text())
 .then(jsonText => {
     const finalJson = JSON.parse(jsonText)
-    console.log(finalJson)
 })
 
 const configuracoes = {
@@ -11,6 +10,6 @@ const configuracoes = {
     aula: "2.1 Javascript",
 }
 
-const stringConfig = JSON.stringify(configuracoes)
+localStorage.config = JSON.stringify(configuracoes)
 
-console.log(stringConfig)
+console.log(localStorage.config)
