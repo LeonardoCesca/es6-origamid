46b7ba8a6731e6147b74b17025ae44e843543afa
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-24 14:59:26 -0300

    ADD difference between async / then

--- javascript-assincrono/async-await/script.js
+++ javascript-assincrono/async-await/script.js
@@ -1,8 +1,18 @@
-async function puxarDados() {
-    const responseDados = await fetch('./dados.json');
-    const jsonDados = await responseDados.json()
+// Diferença entre async / then
 
-    document.body.innerText = jsonDados.aula
-}
-
-puxarDados()
+function iniciarFetch() {
+    fetch('./dados.json')
+    .then(dadosResponse => dadosResponse.json())
+    .then(dadosJSON => {
+      document.body.innerText = dadosJSON.titulo;
+    })
+  }
+  iniciarFetch();
+  
+  async function iniciarAsync() {
+    const dadosResponse = await fetch('./dados.json');
+    const dadosJSON = await dadosResponse.json();
+    document.body.innerText = dadosJSON.titulo;
+  }
+  iniciarAsync();
+  
