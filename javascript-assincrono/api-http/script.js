e2fb119c390483813884e2ca1625b874a6909c96
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-24 14:36:25 -0300

    ADD methods http

--- javascript-assincrono/api-http/script.js
+++ javascript-assincrono/api-http/script.js
@@ -1,5 +1,30 @@
+/*
+METHODS: 
+
+- GET -> Puxa informações, utilizado para pegar posts, usuários
+
+- POST -> Utilizado para criar posts, usuários
+
+- PUT -> Geralmente utilizado para atualizar informações
+
+- DELETE -> Deleta uma informação
+*/
+
 fetch('https://pokeapi.co/api/v2/pokemon/')
 .then(response => response.json())
 .then(pokemon => {
     console.log(pokemon)
 })
+
+const url = 'https://jsonplaceholder.typicode.com/posts/';
+const options = {
+    method: 'POST',
+    body: '{"title: "Javascript"}',
+    headers: {
+        "Content-Type": "application/json; charset=utf-8"
+    }
+}
+
+fetch(url, options)
+.then(response => response.json())
+.then(json => console.log(json))
