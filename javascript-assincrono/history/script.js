301bbf65c26ac2ecbaa318d49a4fe114befb0c18
LeonardoCesca <leonardocescaf@gmail.com>
2019-08-25 15:29:09 -0300

    ADD method to get url with click

--- javascript-assincrono/history/script.js
+++ javascript-assincrono/history/script.js
@@ -1,1 +1,14 @@
-console.log(window.history.back())
+const links = document.querySelectorAll('a');
+
+function handleClick(event) {
+    event.preventDefault();
+    fetchPage(event.target.href)
+}
+
+function fetchPage(url) {
+    console.log(url)
+}
+
+links.forEach(link => {
+    link.addEventListener('click', handleClick)
+})
